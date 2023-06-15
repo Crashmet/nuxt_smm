b34e8a2b29977c1bda8d007165005a15f9df75a8
Crashmet <shoma.mtv@gmail.com>
2023-06-15 12:08:29 +0300

    add advertiser brofile

--- pages/account/advertiser.profile.vue
+++ pages/account/advertiser.profile.vue
@@ -1,94 +1,131 @@
 <template>
-  <div class="advertiser-finance">
-    <p class="advertiser-finance__link">Изменить профиль</p>
-
-    <div class="advertiser-finance__body body">
-      <ul class="body__row">
-        <li class="body__cell body__cell-label">
-          <span class="body__label">Рекламодатель</span>
-        </li>
-
-        <li class="body__cell">
-          <input type="text" class="body__input" />
-        </li>
-      </ul>
-
-      <ul class="body__row">
-        <li class="body__cell body__cell-label">
-          <span class="body__label">ИНН</span>
-        </li>
-
-        <li class="body__cell">
-          <input type="text" class="body__input" />
-        </li>
-      </ul>
-
-      <ul class="body__row">
-        <li class="body__cell body__cell-label">
-          <span class="body__label">Адрес</span>
-        </li>
-
-        <li class="body__cell">
-          <input type="text" class="body__input" />
-        </li>
-      </ul>
-
-      <ul class="body__row">
-        <li class="body__cell body__cell-label">
-          <span class="body__label">Телефон</span>
-        </li>
-
-        <li class="body__cell">
-          <input type="text" class="body__input" />
-        </li>
-      </ul>
-
+  <div class="advertiser-profile">
+    <div
+      class="advertiser-profile__body body"
+      v-if="!isOpenModalChangeData && !isOpenModalChangePassword"
+    >
       <ul class="body__row">
         <li class="body__cell body__cell-label">
           <span class="body__label">E-mail</span>
         </li>
 
         <li class="body__cell">
-          <input type="text" class="body__input" />
+          <p type="email" class="body__input">{{ advertiserData.email }}</p>
         </li>
       </ul>
 
       <ul class="body__row">
+        <li class="body__cell body__cell-label">
+          <span class="body__label">Имя</span>
+        </li>
+
+        <li class="body__cell">
+          <p type="text" class="body__input">{{ advertiserData.first_name }}</p>
+        </li>
+      </ul>
+
+      <ul class="body__row">
+        <li class="body__cell body__cell-label">
+          <span class="body__label">Фамилия</span>
+        </li>
+
+        <li class="body__cell">
+          <p type="text" class="body__input">{{ advertiserData.last_name }}</p>
+        </li>
+      </ul>
+
+      <ul class="body__row body__row_long">
         <li class="body__cell body__cell-label">
           <span class="body__label">Website</span>
         </li>
 
         <li class="body__cell">
-          <input type="text" class="body__input" />
+          <ul class="body__cell-list">
+            <li class="body__cell-item">
+              <p type="text" class="body__input cell-item__input"></p>
+            </li>
+          </ul>
         </li>
       </ul>
 
-      <ul class="body__row">
+      <ul class="body__row body__row_long">
         <li class="body__cell body__cell-label">
-          <span class="body__label">Сменить пароль</span>
+          <span class="body__label">ИНН</span>
         </li>
 
         <li class="body__cell">
-          <input type="text" class="body__input" />
+          <ul class="body__cell-list">
+            <li class="body__cell-item">
+              <p type="text" class="body__input cell-item__input"></p>
+            </li>
+          </ul>
         </li>
       </ul>
 
-      <ul class="body__row">
+      <ul class="body__row body__row_long">
         <li class="body__cell body__cell-label">
-          <span class="body__label">Рекламодатель</span>
+          <span class="body__label">Адрес</span>
         </li>
 
         <li class="body__cell">
-          <input type="text" class="body__input" />
+          <ul class="body__cell-list">
+            <li class="body__cell-item">
+              <p type="text" class="body__input cell-item__input"></p>
+            </li>
+          </ul>
         </li>
       </ul>
+
+      <nav class="body-change__row">
+        <button class="body-change__btn" @click="changeDataModalStatus(true)">
+          <span class="body-change__text">Изменить профиль</span>
+        </button>
+
+        <button
+          class="body-change__btn"
+          @click="changePasswordModalStatus(true)"
+        >
+          <span class="body-change__text">Изменить пароль</span>
+        </button>
+      </nav>
     </div>
+    <change-advertiser-profile-data v-else-if="isOpenModalChangeData" />
+    <change-advertiser-profile-password v-else-if="isOpenModalChangePassword" />
   </div>
 </template>
 
 <script>
+import { mapActions, mapGetters } from "vuex";
+
+import changeAdvertiserProfileData from "~/components/modal/changeAdvertiserProfileData.vue";
+import changeAdvertiserProfilePassword from "~/components/modal/changeAdvertiserProfilePassword.vue";
+
 export default {
-  name: 'AdvertiserFinance',
+  name: "AdvertiserProfile",
+
+  components: { changeAdvertiserProfileData, changeAdvertiserProfilePassword },
+
+  mounted() {
+    this.addAdvertiserData();
+  },
+
+  computed: {
+    ...mapGetters({
+      advertiserData: "advertiserProfileStore/advertiserData",
+      isOpenModalChangeData: "advertiserProfileStore/isOpenModalChangeData",
+      isOpenModalChangePassword:
+        "advertiserProfileStore/isOpenModalChangePassword",
+    }),
+  },
+
+  methods: {
+    ...mapActions({
+      addAdvertiserData: "advertiserProfileStore/addAdvertiserData",
+      changeDataModalStatus: "advertiserProfileStore/changeDataModalStatus",
+      changePasswordModalStatus:
+        "advertiserProfileStore/changePasswordModalStatus",
+    }),
+  },
 };
 </script>
 
@@ -99,13 +136,8 @@ export default {
 * Autoprefixer: v10.4.7
 * Browsers: last 4 version
 */
-
-.advertiser-finance__link {
-  margin-bottom: 1.6667rem;
-  -webkit-text-decoration-line: underline;
-  text-decoration-line: underline;
-  color: #ff3600;
-  text-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+.advertiser-profile {
+  position: relative;
 }
 
 .body__row {
@@ -117,7 +149,7 @@ export default {
   align-items: center;
 }
 
-.body__row:not(:last-child) {
+.body__row {
   margin-bottom: 0.8889rem;
 }
 
@@ -127,8 +159,47 @@ export default {
 }
 
 .body__input {
-  padding-left: 0.6111rem;
-  max-width: 9.8333rem;
+  padding: 0.2778rem 0.6111rem;
+  min-width: 8.3889rem;
+  width: 100%;
+  min-height: 1.6667rem;
+  font-size: 0.8889rem;
+  line-height: 1rem;
+  text-align: center;
+  color: rgba(13, 13, 13, 0.61);
+  border: 1px solid rgba(240, 158, 86, 0.58);
+  border-radius: 10px;
+  background-color: transparent;
+  /* -webkit-transition: border 0.3s ease;
+  -o-transition: border 0.3s ease;
+  transition: border 0.3s ease; */
+}
+/* 
+.body__input:hover,
+.body__input:active {
+  border: 1.5px solid rgba(240, 158, 86, 0.94);
+  -webkit-transition: border 0.3s ease;
+  -o-transition: border 0.3s ease;
+  transition: border 0.3s ease;
+} */
+
+.body__cell-list {
+  display: -webkit-box;
+  display: -ms-flexbox;
+  display: flex;
+  -webkit-box-align: center;
+  -ms-flex-align: center;
+  align-items: center;
+  -ms-flex-wrap: wrap;
+  flex-wrap: wrap;
+}
+
+.body__cell-item:not(:last-child) {
+  margin-right: 0.9rem;
+}
+
+.cell-item__input_price {
+  max-width: 6.9444rem;
   width: 100%;
   min-height: 1.6667rem;
   font-size: 0.8889rem;
@@ -137,35 +208,141 @@ export default {
   border: 1px solid rgba(240, 158, 86, 0.58);
   border-radius: 10px;
   background-color: transparent;
+  text-align: center;
   -webkit-transition: border 0.3s ease;
   -o-transition: border 0.3s ease;
   transition: border 0.3s ease;
 }
 
-.body__input:hover,
-.body__input:active {
+.cell-item__input_price:hover,
+.cell-item__input_price:active {
   border: 1.5px solid rgba(240, 158, 86, 0.94);
   -webkit-transition: border 0.3s ease;
   -o-transition: border 0.3s ease;
   transition: border 0.3s ease;
 }
 
+.cell-item__btn {
+  padding: 6px 8px;
+  max-width: 19.4444rem;
+  width: 100%;
+  min-height: 1.6667rem;
+  font-size: 0.7778rem;
+  line-height: 0.8889rem;
+  background: #f09e56;
+  border: 1px solid rgba(240, 158, 86, 0.58);
+  border-radius: 10px;
+}
+
+.body__cell-item__link {
+  text-align: center;
+  max-width: 216px;
+}
+
+.cell-item__link {
+  font-size: 0.8889rem;
+  line-height: 1.0556rem;
+  -webkit-text-decoration-line: underline;
+  text-decoration-line: underline;
+}
+
+.body-change__row {
+  display: -webkit-box;
+  display: -ms-flexbox;
+  display: flex;
+  flex-wrap: wrap;
+
+  margin-top: 2.5rem;
+}
+
+.body-change__btn {
+  margin-bottom: 0.5556rem;
+  padding: 0.2222rem 0.5556rem;
+  background: #f09e56;
+  border: 1px solid rgba(240, 158, 86, 0.58);
+  border-radius: 10px;
+  background-color: transparent;
+  -webkit-transition: border 0.3s ease;
+  -o-transition: border 0.3s ease;
+  transition: border 0.3s ease;
+}
+
+.body-change__btn:not(:last-child) {
+  margin-right: 2.7778rem;
+}
+
+.body-change__btn:hover,
+.body-change__btn:active {
+  border: 1.5px solid rgba(240, 158, 86, 0.94);
+  -webkit-transition: border 0.3s ease;
+  -o-transition: border 0.3s ease;
+  transition: border 0.3s ease;
+}
+
+.body-change__text {
+  font-size: 0.8889rem;
+  line-height: 1.0556rem;
+  color: rgba(13, 13, 13, 0.61);
+}
+
+.body-change__btn:hover > .body-change__text,
+.body-change__btn:active > .body-change__text {
+  color: #ff3600;
+  -webkit-transition: all 0.3s ease;
+  -o-transition: all 0.3s ease;
+  transition: all 0.3s ease;
+}
+
+.body-change__text_active {
+  color: #ff3600;
+  /* text-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25); */
+  text-decoration-line: underline;
+  -webkit-transition: all 0.3s ease;
+  -o-transition: all 0.3s ease;
+  transition: all 0.3s ease;
+}
+
 @media (max-width: 1175px) {
-  .body__row:not(:last-child) {
+  .body__row_long {
+    -webkit-box-align: start;
+    -ms-flex-align: start;
+    align-items: start;
+  }
+  .body__row {
     margin-bottom: 15px;
+  }
+
+  .body__cell-item {
+    margin-bottom: 9px;
   }
 }
 
 @media (max-width: 480px) {
-
-  .body__row:not(:last-child) {
+  .body__row {
     margin-bottom: 13px;
   }
 
+  .body__cell-item:not(:last-child) {
+    margin-right: 6px;
+  }
+
   .body__input {
-    max-width: 88px;
-    width: 100%;
+    /* min-width: 0; */
+    overflow: scroll;
+    max-width: 170px;
     border-radius: 5px;
+  }
+
+  .cell-item__btn {
+    border-radius: 5px;
+    padding: 4px 4px;
+    max-width: 104px;
+    width: 100%;
+  }
+
+  .body__cell-item__link {
+    max-width: 87px;
+    width: 100%;
   }
 }
 </style>
---
+++ store/advertiserProfileStore.js
@@ -0,0 +1,96 @@
+export const state = () => ({
+  advertiserData: {
+    username: "",
+    first_name: "",
+    last_name: "",
+    email: " ",
+    role: "",
+  },
+
+  isOpenModalChangeData: false,
+  isOpenModalChangePassword: false,
+
+  validatorResponse: {},
+});
+
+export const getters = {
+  advertiserData: ({ advertiserData }) => advertiserData,
+
+  isOpenModalChangeData: ({ isOpenModalChangeData }) => isOpenModalChangeData,
+
+  isOpenModalChangePassword: ({ isOpenModalChangePassword }) =>
+    isOpenModalChangePassword,
+
+  validatorResponse: ({ validatorResponse }) => validatorResponse,
+};
+
+export const mutations = {
+  ADD_ADVERTISER_DATA(state, response) {
+    const { username, first_name, last_name, email, role } = response;
+
+    const advertiserData = { username, first_name, last_name, email, role };
+
+    state.advertiserData = advertiserData;
+  },
+
+  CHANGE_DATA_MODAL_STATUS(state, flag) {
+    state.isOpenModalChangeData = flag;
+  },
+
+  CHANGE_PASSWORD_MODAL_STATUS(state, flag) {
+    state.isOpenModalChangePassword = flag;
+  },
+
+  SET_VALIDATOR_DATA(state, validatorResponse) {
+    state.validatorResponse = validatorResponse;
+  },
+};
+
+export const actions = {
+  changeDataModalStatus({ commit }, flag) {
+    commit("CHANGE_DATA_MODAL_STATUS", flag);
+  },
+
+  changePasswordModalStatus({ commit }, flag) {
+    commit("CHANGE_PASSWORD_MODAL_STATUS", flag);
+  },
+
+  async addAdvertiserData({ commit }) {
+    await this.$axios
+      .$get("account/")
+      .then((response) => {
+        commit("ADD_ADVERTISER_DATA", response);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  },
+
+  async changeAdvertiserData({ commit }, advertiserData) {
+    const dataJson = JSON.stringify(advertiserData);
+
+    await this.$axios
+      .$patch("account/", dataJson)
+      .then((response) => {
+        location.reload();
+      })
+      .catch((error) => {
+        console.log(error.response);
+      });
+  },
+
+  async changeAdvertiserPassword({ commit }, passwordData) {
+    const dataJson = JSON.stringify(passwordData);
+
+    await this.$axios
+      .$put("account/password_change/", dataJson)
+      .then((response) => {
+        commit("SET_VALIDATOR_DATA", {});
+
+        location.reload();
+      })
+      .catch((error) => {
+        commit("SET_VALIDATOR_DATA", error.response.data);
+      });
+  },
+};
