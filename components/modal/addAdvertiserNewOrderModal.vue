806c4e00160f7cdc82875c1c00afc81e43a9b212
Crashmet <shoma.mtv@gmail.com>
2023-07-11 14:27:37 +0300

    test

--- components/modal/addAdvertiserNewOrderModal.vue
+++ components/modal/addAdvertiserNewOrderModal.vue
@@ -201,7 +201,7 @@
 
     <b-row class="pt-1 mt-4">
       <b-col cols="0" md="3" lg="2"></b-col>
-      <b-col cols="3" md="2">
+      <b-col cols="2" md="2">
         <b-button
           class="btn btn-dark btn_back rounded mb-3 px-4"
           type="button"
@@ -210,7 +210,7 @@
           Назад
         </b-button></b-col
       >
-      <b-col cols="5" md="4" lg="3">
+      <b-col cols="4" md="4" lg="3">
         <b-button
           class="btn btn-dark btn_change rounded mb-3 px-4"
           type="button"
