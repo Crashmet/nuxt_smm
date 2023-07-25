2dfc165759cd210c81acd6e760f2f9ea24afdbac
Crashmet <shoma.mtv@gmail.com>
2023-07-25 12:12:37 +0300

    test profiles

--- pages/account/advertiser.profile.vue
+++ pages/account/advertiser.profile.vue
@@ -1,134 +1,97 @@
 <template>
   <div>
     <div
-      class="advertiser-profile__body body"
       v-if="!isOpenModalChangeData && !isOpenModalChangePassword"
+      class="container py-4"
     >
-      <b-form-group
-        id="fieldset-horizontal"
-        label-cols-sm="4"
-        label-cols-lg="2"
-        content-cols-sm
-        content-cols-lg="4"
-        label="E-mail"
-        label-for="input-email"
-      >
-        <b-form-input
-          id="input-email"
-          disabled
-          v-model="advertiserData.email"
-        ></b-form-input>
-      </b-form-group>
+      <div class="row d-flex">
+        <div>
+          <div class="card mb-3" style="border-radius: 10px">
+            <div class="row g-0">
+              <div
+                class="col-md-4 gradient-custom d-flex flex-column align-items-center justify-content-center text-white"
+              >
+                <img
+                  src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-chat/ava1-bg.webp"
+                  alt="Avatar"
+                  class="img-fluid my-5"
+                  style="width: 120px"
+                />
+                <h4>{{ advertiserData.nickname }}</h4>
+                <h5>
+                  {{ advertiserData.first_name }} {{ advertiserData.last_name }}
+                </h5>
+                <p>Blogger</p>
+                <b-button
+                  class="my-4 bg-transparent border-0"
+                  type="button"
+                  @click.prevent="changeDataModalStatus(true)"
+                >
+                  <i class="fa fa-edit" style="font-size: 22px"></i>
+                </b-button>
+              </div>
+              <div class="col-md-8">
+                <div class="card-body p-4">
+                  <h5>Information</h5>
+                  <hr class="mt-0 mb-4" />
+                  <div class="row pt-1">
+                    <div class="col-6 mb-3">
+                      <h6>Email</h6>
+                      <p class="text-muted">{{ advertiserData.email }}</p>
+                    </div>
+                    <div class="col-6 mb-3">
+                      <h6>Phone</h6>
+                      <p class="text-muted">+123 456 789</p>
+                    </div>
+                  </div>
+                  <h6>Projects</h6>
+                  <hr class="mt-0 mb-4" />
+                  <div class="row pt-1">
+                    <div class="col-6 mb-3">
+                      <h6>Recent</h6>
+                      <p class="text-muted">Lorem ipsum</p>
+                    </div>
+                    <div class="col-6 mb-3">
+                      <h6>Most Viewed</h6>
+                      <p class="text-muted">Dolor sit amet</p>
+                    </div>
+                  </div>
+                  <div class="d-flex justify-content-start mt-4">
+                    <a href="#!"
+                      ><i
+                        class="fa fa-facebook-f fa-lg me-3"
+                        style="font-size: 20px"
+                      ></i
+                    ></a>
+                    <a href="#!"
+                      ><i
+                        class="fa fa-twitter fa-lg me-3"
+                        style="font-size: 20px"
+                      ></i
+                    ></a>
+                    <a href="#!"
+                      ><i
+                        class="fa fa-instagram fa-lg"
+                        style="font-size: 20px"
+                      ></i
+                    ></a>
+                  </div>
 
-      <b-form-group
-        id="fieldset-horizontal"
-        label-cols-sm="4"
-        label-cols-lg="2"
-        content-cols-sm
-        content-cols-lg="4"
-        label="Имя"
-        label-for="input-firstName"
-      >
-        <b-form-input
-          id="input-firstName"
-          disabled
-          v-model="advertiserData.first_name"
-        ></b-form-input>
-      </b-form-group>
-
-      <b-form-group
-        id="fieldset-horizontal"
-        label-cols-sm="4"
-        label-cols-lg="2"
-        content-cols-sm
-        content-cols-lg="4"
-        label="Имя"
-        label-for="input-firstName"
-      >
-        <b-form-input
-          id="input-firstName"
-          disabled
-          v-model="advertiserData.first_name"
-        ></b-form-input>
-      </b-form-group>
-
-      <b-form-group
-        id="fieldset-horizontal"
-        label-cols-sm="4"
-        label-cols-lg="2"
-        content-cols-sm
-        content-cols-lg="4"
-        label="Фамилия"
-        label-for="input-lastName"
-      >
-        <b-form-input
-          id="input-lastName"
-          disabled
-          v-model="advertiserData.last_name"
-        ></b-form-input>
-      </b-form-group>
-
-      <b-form-group
-        id="fieldset-horizontal"
-        label-cols-sm="4"
-        label-cols-lg="2"
-        content-cols-sm
-        content-cols-lg="4"
-        label="Website"
-        label-for="input-website"
-      >
-        <b-form-input
-          id="input-website"
-          disabled
-          placeholder="..."
-        ></b-form-input>
-      </b-form-group>
-
-      <b-form-group
-        id="fieldset-horizontal"
-        label-cols-sm="4"
-        label-cols-lg="2"
-        content-cols-sm
-        content-cols-lg="4"
-        label="ИНН"
-        label-for="input-inn"
-      >
-        <b-form-input id="input-inn" disabled placeholder="..."></b-form-input>
-      </b-form-group>
-
-      <b-form-group
-        id="fieldset-horizontal"
-        label-cols-sm="4"
-        label-cols-lg="2"
-        content-cols-sm
-        content-cols-lg="4"
-        label="Адрес"
-        label-for="input-adress"
-      >
-        <b-form-input
-          id="input-adress"
-          disabled
-          placeholder="..."
-        ></b-form-input>
-      </b-form-group>
-
-      <b-button-group class="pt-1 mt-4">
-        <b-button
-          class="btn btn-dark mr-5 rounded"
-          type="button"
-          @click.prevent="changeDataModalStatus(true)"
-        >
-          Изменить профиль
-        </b-button>
-        <b-button
-          class="btn btn-dark rounded"
-          type="button"
-          @click.prevent="changePasswordModalStatus(true)"
-        >
-          Изменить пароль
-        </b-button>
-      </b-button-group>
+                  <b-button
+                    class="mt-5 btn btn-dark rounded"
+                    type="button"
+                    @click.prevent="changePasswordModalStatus(true)"
+                  >
+                    Изменить пароль
+                  </b-button>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
+
     <change-advertiser-profile-data v-else-if="isOpenModalChangeData" />
     <change-advertiser-profile-password v-else-if="isOpenModalChangePassword" />
   </div>
@@ -169,4 +132,25 @@ export default {
 };
 </script>
 
-<style scoped></style>
+<style scoped>
+.gradient-custom {
+  /* fallback for old browsers */
+  background: #f6d365;
+  border-top-left-radius: 10px;
+  border-bottom-left-radius: 10px;
+
+  /* Chrome 10-25, Safari 5.1-6 */
+  background: -webkit-linear-gradient(
+    to right bottom,
+    rgba(246, 211, 101, 1),
+    rgba(253, 160, 133, 1)
+  );
+
+  /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
+  background: linear-gradient(
+    to right bottom,
+    rgba(246, 211, 101, 1),
+    rgba(253, 160, 133, 1)
+  );
+}
+</style>
--- pages/account/blogger.profile.vue
+++ pages/account/blogger.profile.vue
@@ -1,122 +1,97 @@
 <template>
   <div>
     <div
-      class="blogger-profile__body body"
       v-if="!isOpenModalChangeData && !isOpenModalChangePassword"
+      class="container py-4"
     >
-      <b-form-group
-        id="fieldset-horizontal"
-        label-cols-sm="4"
-        label-cols-lg="2"
-        content-cols-sm
-        content-cols-lg="4"
-        label="E-mail"
-        label-for="input-email"
-      >
-        <b-form-input
-          id="input-email"
-          disabled
-          v-model="bloggerData.email"
-        ></b-form-input>
-      </b-form-group>
+      <div class="row d-flex">
+        <div>
+          <div class="card mb-3" style="border-radius: 10px">
+            <div class="row g-0">
+              <div
+                class="col-md-4 gradient-custom d-flex flex-column align-items-center justify-content-center text-white"
+              >
+                <img
+                  src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-chat/ava1-bg.webp"
+                  alt="Avatar"
+                  class="img-fluid my-5"
+                  style="width: 120px"
+                />
+                <h4>{{ bloggerData.nickname }}</h4>
+                <h5>
+                  {{ bloggerData.first_name }} {{ bloggerData.last_name }}
+                </h5>
+                <p>Blogger</p>
+                <b-button
+                  class="my-4 bg-transparent border-0"
+                  type="button"
+                  @click.prevent="changeDataModalStatus(true)"
+                >
+                  <i class="fa fa-edit" style="font-size: 22px"></i>
+                </b-button>
+              </div>
+              <div class="col-md-8">
+                <div class="card-body p-4">
+                  <h5>Information</h5>
+                  <hr class="mt-0 mb-4" />
+                  <div class="row pt-1">
+                    <div class="col-6 mb-3">
+                      <h6>Email</h6>
+                      <p class="text-muted">{{ bloggerData.email }}</p>
+                    </div>
+                    <div class="col-6 mb-3">
+                      <h6>Phone</h6>
+                      <p class="text-muted">+123 456 789</p>
+                    </div>
+                  </div>
+                  <h6>Projects</h6>
+                  <hr class="mt-0 mb-4" />
+                  <div class="row pt-1">
+                    <div class="col-6 mb-3">
+                      <h6>Recent</h6>
+                      <p class="text-muted">Lorem ipsum</p>
+                    </div>
+                    <div class="col-6 mb-3">
+                      <h6>Most Viewed</h6>
+                      <p class="text-muted">Dolor sit amet</p>
+                    </div>
+                  </div>
+                  <div class="d-flex justify-content-start mt-4">
+                    <a href="#!"
+                      ><i
+                        class="fa fa-facebook-f fa-lg me-3"
+                        style="font-size: 20px"
+                      ></i
+                    ></a>
+                    <a href="#!"
+                      ><i
+                        class="fa fa-twitter fa-lg me-3"
+                        style="font-size: 20px"
+                      ></i
+                    ></a>
+                    <a href="#!"
+                      ><i
+                        class="fa fa-instagram fa-lg"
+                        style="font-size: 20px"
+                      ></i
+                    ></a>
+                  </div>
 
-      <b-form-group
-        id="fieldset-horizontal"
-        label-cols-sm="4"
-        label-cols-lg="2"
-        content-cols-sm
-        content-cols-lg="4"
-        label="Имя"
-        label-for="input-firstName"
-      >
-        <b-form-input
-          id="input-firstName"
-          disabled
-          v-model="bloggerData.first_name"
-        ></b-form-input>
-      </b-form-group>
-
-      <b-form-group
-        id="fieldset-horizontal"
-        label-cols-sm="4"
-        label-cols-lg="2"
-        content-cols-sm
-        content-cols-lg="4"
-        label="Имя"
-        label-for="input-firstName"
-      >
-        <b-form-input
-          id="input-firstName"
-          disabled
-          v-model="bloggerData.first_name"
-        ></b-form-input>
-      </b-form-group>
-
-      <b-form-group
-        id="fieldset-horizontal"
-        label-cols-sm="4"
-        label-cols-lg="2"
-        content-cols-sm
-        content-cols-lg="4"
-        label="Фамилия"
-        label-for="input-lastName"
-      >
-        <b-form-input
-          id="input-lastName"
-          disabled
-          v-model="bloggerData.last_name"
-        ></b-form-input>
-      </b-form-group>
-
-      <b-form-group
-        id="fieldset-horizontal"
-        label-cols-sm="4"
-        label-cols-lg="2"
-        content-cols-sm
-        content-cols-lg="4"
-        label="Instagram"
-        label-for="input-instagram"
-      >
-        <b-form-input
-          id="input-instagram"
-          disabled
-          placeholder="nickname."
-        ></b-form-input>
-      </b-form-group>
-
-      <b-form-group
-        id="fieldset-horizontal"
-        label-cols-sm="4"
-        label-cols-lg="2"
-        content-cols-sm
-        content-cols-lg="4"
-        label="Tiktok"
-        label-for="input-tiktok"
-      >
-        <b-form-input
-          id="input-tiktok"
-          disabled
-          placeholder="nickname."
-        ></b-form-input>
-      </b-form-group>
-
-      <b-button-group class="pt-1 mt-4">
-        <b-button
-          class="btn btn-dark mr-5 rounded"
-          type="button"
-          @click.prevent="changeDataModalStatus(true)"
-        >
-          Изменить профиль
-        </b-button>
-        <b-button
-          class="btn btn-dark rounded"
-          type="button"
-          @click.prevent="changePasswordModalStatus(true)"
-        >
-          Изменить пароль
-        </b-button>
-      </b-button-group>
+                  <b-button
+                    class="mt-5 btn btn-dark rounded"
+                    type="button"
+                    @click.prevent="changePasswordModalStatus(true)"
+                  >
+                    Изменить пароль
+                  </b-button>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
+
     <change-blogger-profile-data v-else-if="isOpenModalChangeData" />
     <change-blogger-profile-password v-else-if="isOpenModalChangePassword" />
   </div>
@@ -157,4 +132,25 @@ export default {
 };
 </script>
 
-<style scoped></style>
+<style scoped>
+.gradient-custom {
+  /* fallback for old browsers */
+  background: #f6d365;
+  border-top-left-radius: 10px;
+  border-bottom-left-radius: 10px;
+
+  /* Chrome 10-25, Safari 5.1-6 */
+  background: -webkit-linear-gradient(
+    to right bottom,
+    rgba(246, 211, 101, 1),
+    rgba(253, 160, 133, 1)
+  );
+
+  /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
+  background: linear-gradient(
+    to right bottom,
+    rgba(246, 211, 101, 1),
+    rgba(253, 160, 133, 1)
+  );
+}
+</style>
